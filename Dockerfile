12663228efd1b597f67032d3498f63ca2a32c297
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-07-26 08:49:16 +0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+FROM alpine:3.14
+
+MAINTAINER ilia.dmitriev@gmail.com
+
+RUN set -xe \
+    
+# Install postgres, python3, runit
+    && apk add --no-cache musl-locales python3 postgresql py3-pip runit \
+    && mkdir -p /run/postgresql \
+    && chown -R postgres:postgres /run/postgresql \
+    && mkdir -p /var/lib/posgtresql/data \
+    && chown postgres:postgres /var/lib/posgtresql/data \
+
+# Install build dependencies
+    && apk add --no-cache --virtual .build-deps \
+            build-base python3-dev \
+            linux-headers postgresql-dev \
+            
+# Install patroni, psycopg2
+    && pip install --no-cache-dir patroni psycopg2 requests patroni[etcd] \
+    
+# Install etcd 
+    && case $(uname -m) in \
+        "aarch64") \
+            wget https://github.com/etcd-io/etcd/releases/download/v3.5.0/etcd-v3.5.0-linux-arm64.tar.gz \
+                -O /tmp/etcd-v3.5.0.tar.gz \
+            ;; \
+        "amd64") \
+            wget https://github.com/etcd-io/etcd/releases/download/v3.5.0/etcd-v3.5.0-linux-amd64.tar.gz \
+            -O /tmp/etcd-v3.5.0.tar.gz \
+            ;; \
+        esac \
+    && mkdir /opt/etcd \
+    && tar -xvf /tmp/etcd-v3.5.0.tar.gz -C /opt/etcd --strip-components=1 \
+    
+# cleanup
+    && rm -rf /tmp/* \
+    && apk del .build-deps \
+    && find /var/log -type f -exec truncate --size 0 {} \;
+    
+ENV PATH=$PATH:/opt/etcd \
+    LANG=ru_RU.UTF-8 \
+    PGDATA=/var/lib/postgresql/data
+
+COPY postgres0.yml /etc/patroni/postgres0.yml
+COPY etcd-cluster.py /opt/etcd/etcd-cluster.py
+
+RUN chown -R postgres:postgres /etc/patroni /opt/etcd
+
+STOPSIGNAL SIGINT
+
+
+EXPOSE 5432 8008 8080 2379 2380
+
+CMD ["python3", "/opt/etcd/etcd-cluster.py"]
---
+++ etcd-cluster.py
@@ -0,0 +1,138 @@
+from os import environ as env
+import os
+import requests
+import socket
+import sys
+import subprocess
+import shutil
+import signal
+
+
+timeout = 10
+
+
+def preexec_function():
+    os.setpgrp()
+    # signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+
+def interrupt_handler(signum, frame):
+    print("Press ctrl+C")
+
+    if member_id:
+        print(f"Removing member {INSTANCE_ID} {INSTANCE_IP} {member_id} from cluster")
+        # curl http://10.0.0.10:2379/v2/members/272e204152 -XDELETE
+        res = requests.delete(
+            url=CLIENT_REQUEST_URL + f'/{member_id}',
+            timeout=timeout
+        )
+        if res.status_code == 204:
+            print(f"Removed member {INSTANCE_ID}")
+        else:
+            print(res.request.url, res.request.headers, res.request.body)
+            print(res, res.text)
+    else:
+        exit()
+
+
+signal.signal(signal.SIGINT, interrupt_handler)
+
+member_id = None
+
+SECRET_TOKEN = env.get('SECRET_TOKEN', 'test_token')
+
+CLIENT_PORT = env.get('CLIENT_PORT', 2379)
+SERVER_PORT = env.get('SERVER_PORT', 2380)
+
+INSTANCE_ID = env.get('POD_NAME', socket.gethostname())
+INSTANCE_IP = env.get('POD_IP', '127.0.0.1')
+
+DATA_DIR = env.get('DATA_DIR', f"data/{INSTANCE_ID}")
+
+CLIENT_SCHEME = env.get('CLIENT_SCHEME', 'http')
+PEER_SCHEME = env.get('PEER_SCHEME', 'http')
+
+CLIENT_REQUEST_HOST = env.get('CLIENT_REQUEST_HOST', '127.0.0.1')
+
+CLIENT_REQUEST_URL = f"{CLIENT_SCHEME}://{CLIENT_REQUEST_HOST}:{CLIENT_PORT}/v2/members"
+
+cluster_found = False
+
+try:
+    peer_urls_response = requests.get(url=CLIENT_REQUEST_URL, timeout=timeout)
+    response = peer_urls_response.json()
+    cluster_found = True
+except requests.exceptions.ConnectionError:
+    response = None
+
+if not cluster_found:
+    if os.path.exists(DATA_DIR):
+        shutil.rmtree(DATA_DIR)
+
+    command = [
+        "etcd",
+        "--data-dir", DATA_DIR,
+        "--enable-v2",
+        "--name", f"{INSTANCE_ID}",
+        "--listen-client-urls", f"{CLIENT_SCHEME}://{INSTANCE_IP}:{CLIENT_PORT}",
+        "--advertise-client-urls", f"{CLIENT_SCHEME}://{INSTANCE_IP}:{CLIENT_PORT}",
+        "--listen-peer-urls", f"{CLIENT_SCHEME}://{INSTANCE_IP}:{SERVER_PORT}",
+        "--initial-advertise-peer-urls", f"{CLIENT_SCHEME}://{INSTANCE_IP}:{SERVER_PORT}",
+        "--initial-cluster-token", f"{SECRET_TOKEN}",
+        "--initial-cluster-state", "new"
+    ]
+    subprocess.call(command, stdout=sys.stdout, preexec_fn=preexec_function)
+
+else:
+
+    initial_cluster = []
+
+    for member in response.get('members'):
+        if member.get('name') and member.get('peerURLs') \
+                and len(member.get('peerURLs')):
+            name = member.get('name')
+            peer_url_first = member.get('peerURLs').pop()
+            initial_cluster.append(f'{name}={peer_url_first}')
+
+    initial_cluster.append(f'{INSTANCE_ID}={CLIENT_SCHEME}://{INSTANCE_IP}:{SERVER_PORT}')
+
+    print(f"initial_cluster={initial_cluster}")
+
+    client_url = CLIENT_REQUEST_URL
+
+    add_response = requests.post(
+        url=client_url,
+        json={
+            'peerURLs': [
+                f"{CLIENT_SCHEME}://{INSTANCE_IP}:{SERVER_PORT}"
+            ],
+            'name': INSTANCE_ID
+        },
+        timeout=timeout
+    )
+
+    if add_response.status_code != 201:
+        print("Error")
+        print(add_response.status_code)
+        print(add_response.text)
+    else:
+        member_id = add_response.json().get('id')
+        print('member_id=', member_id)
+
+    if os.path.exists(DATA_DIR):
+        shutil.rmtree(DATA_DIR)
+
+    command = [
+        "etcd",
+        "--data-dir", DATA_DIR,
+        "--enable-v2",
+        "--name", f"{INSTANCE_ID}",
+        f"--initial-cluster", ','.join(initial_cluster),
+        "--listen-client-urls", f"{CLIENT_SCHEME}://{INSTANCE_IP}:{CLIENT_PORT}",
+        "--advertise-client-urls", f"{CLIENT_SCHEME}://{INSTANCE_IP}:{CLIENT_PORT}",
+        "--listen-peer-urls", f"{CLIENT_SCHEME}://{INSTANCE_IP}:{SERVER_PORT}",
+        "--initial-advertise-peer-urls", f"{CLIENT_SCHEME}://{INSTANCE_IP}:{SERVER_PORT}",
+        "--initial-cluster-token", f"{SECRET_TOKEN}",
+        "--initial-cluster-state", "existing"
+    ]
+    subprocess.call(command, stdout=sys.stdout, preexec_fn=preexec_function)
